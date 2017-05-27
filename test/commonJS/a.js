80e3bd7ccf8258eafe85b66de65fa525e673ae32
HARRISKING <llc13588440410@outlook.com>
2017-05-27 15:45:34 +0800

    new demo

---
+++ test/commonJS/a.js
@@ -0,0 +1,25 @@
+// 第一次试验成功：
+// var name = "harrisking";
+//
+// function sayName(){
+//   console.log(name);
+// }
+// function sayHello(){
+//   console.log('hello'+' '+name);
+// }
+//
+// module.exports = {
+//   sayHello : sayHello,
+//   sayName : sayName
+// }
+// 第二次实验：
+
+var people = {
+  name: harrisking,
+  age:23,
+  saysth: fuction(){
+    console.log('hello,my name is '+this.name+',my age is '+this.age);
+  }
+}
+
+module.exports = people;
---
+++ test/commonJS/b.js
@@ -0,0 +1,4 @@
+// 第一次试验成功：
+// var nameModule = require('./a.js');
+// nameModule.sayName();
+// nameModule.sayHello();
