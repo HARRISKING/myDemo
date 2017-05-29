90b14457f60b707e31d6810234fe7caa42063954
HARRISKING <llc13588440410@outlook.com>
2017-05-29 13:41:45 +0800

    new

--- test/commonJS/a.js
+++ test/commonJS/a.js
@@ -1,35 +1,32 @@
 // 第一次试验成功：
-// var name = "harrisking";
-//
-// function sayName(){
-//   console.log(name);
-// }
-// function sayHello(){
-//   console.log('hello'+' '+name);
-// }
-//
-// module.exports = {
-//   sayHello : sayHello,
-//   sayName : sayName
-// }
+    var name = "harrisking";
+    function sayName(){
+      console.log(name);
+    }
+    function sayHello(){
+      console.log('hello'+' '+name);
+    }
+    module.exports = {
+      sayHello : sayHello,
+      sayName : sayName
+    }
 // 第二次实验：
+    var people = {
+      name: 'harrisking',
+      age:23,
+      saysth: function(){
+        console.log('hello,my name is '+this.name+',my age is '+this.age);
+      }
+    }
 
-// var people = {
-//   name: 'harrisking',
-//   age:23,
-//   saysth: function(){
-//     console.log('hello,my name is '+this.name+',my age is '+this.age);
-//   }
-// }
-//
-// module.exports = people;
+    module.exports = people;
 
 // commonjs:
 
-// function fuckyou(name){
-//   console.log(name+'you are a big ass!');
-// }
-//
-// module.exports = {
-//   fuckyou : fuckyou
-// }
+function fuckyou(name){
+  console.log(name+'you are a big ass!');
+}
+
+module.exports = {
+  fuckyou : fuckyou
+}
--- test/commonJS/b.js
+++ test/commonJS/b.js
@@ -1,11 +1,11 @@
 // 第一次试验成功：
-// var nameModule = require('./a.js');
-// nameModule.sayName();
-// nameModule.sayHello();
+    var nameModule = require('./a.js');
+    nameModule.sayName();
+    nameModule.sayHello();
 
 // 第二次试验：
-// var info = require('./a.js');
-// info.saysth();
-// commonjs:
-// var fuck = require('./a');
-// fuck.fuckyou();
+    var info = require('./a.js');
+    info.saysth();
+commonjs:
+var fuck = require('./a');
+fuck.fuckyou();
