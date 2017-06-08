d27f448713091da55f1924af471aa7f8111dbb76
HARRISKING <llc13588440410@outlook.com>
2017-06-08 18:38:06 +0800

    6.8

---
+++ test/gulp2/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
+
+gulp.task('default',function(){
+  return gulp.src('./src/css/*.css')
+  .pipe(concat('all.css'))
+  .pipe(cssnano())
+  .pipe(gulp.dest('./dist/'));
+});
