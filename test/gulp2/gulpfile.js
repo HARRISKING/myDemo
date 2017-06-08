8923cfe52561be173571103f8d4e2155e357037e
HARRISKING <llc13588440410@outlook.com>
2017-06-08 20:05:08 +0800

    6.8

--- test/gulp2/gulpfile.js
+++ test/gulp2/gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var cssnano = require('gulp-cssnano');
+    var gulp = require('gulp');
+    var concat = require('gulp-concat');
+    var cssnano = require('gulp-cssnano');
 
-gulp.task('default',function(){
-  return gulp.src('./src/css/*.css')
-  .pipe(concat('all.css'))
-  .pipe(cssnano())
-  .pipe(gulp.dest('./dist/'));
-});
+    gulp.task('build:css',function(){
+      return gulp.src('./src/css/*.css')
+      .pipe(concat('all.css'))
+      .pipe(cssnano())
+      .pipe(gulp.dest('./dist/'));
+    });
